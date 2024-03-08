440505b90c630ef970c9c7015d97029891e27ba7
Max Carlquist <tenkaklet@gmail.com>
2024-03-08 12:58:46 +0100

    started to make program dynamic

--- commands/ip/index.mjs
+++ commands/ip/index.mjs
@@ -4,6 +4,7 @@ import chalk from 'chalk';
 import fetch from 'node-fetch';
 import Table from 'cli-table3';
 import { handleFetchError } from '../../helpers/handleError.mjs';
+import { displayResults } from '../../helpers/display.js';
 
 export async function checkIpAddress() {
     const { ipAddress } = await promptForIPAddress();
@@ -53,41 +54,41 @@ async function fetchDataFromIPDatabase(ipAddress) {
     }
 }
 
-function displayResults(data) {
-    // Display the results in a table
-    const ipData = [data][0];
+// function displayResults(data) {
+//     // Display the results in a table
+//     const ipData = [data][0];
 
-    const table = new Table({
-        head: ['IP Address', 'Flag', 'Country', 'Location', 'Use VPN', 'Latitude', 'Longitude'],
-        colWidths: [20, 10, 15, 20, 10, 15, 15],
-        wordWrap: true,
-    });
+//     const table = new Table({
+//         head: ['IP Address', 'Flag', 'Country', 'Location', 'Use VPN', 'Latitude', 'Longitude'],
+//         colWidths: [20, 10, 15, 20, 10, 15, 15],
+//         wordWrap: true,
+//     });
 
-    // Iterates through the phoneData array and pushes each phone number object's
-    // data into a new row in the table variable to display the results.
-    const ipValues = Object.values(ipData);
+//     // Iterates through the phoneData array and pushes each phone number object's
+//     // data into a new row in the table variable to display the results.
+//     const ipValues = Object.values(ipData);
 
-    // Optimized to directly access the needed values instead of looping
-    let ip_address = ipValues[0];
-    let flag = ipValues[18].emoji;
-    let country = ipValues[7];
-    let city = ipValues[1];
-    let use_vpn = ipValues[16].is_vpn;
-    let latitude = ipValues[14];
-    let longitude = ipValues[15];
+//     // Optimized to directly access the needed values instead of looping
+//     let ip_address = ipValues[0];
+//     let flag = ipValues[18].emoji;
+//     let country = ipValues[7];
+//     let city = ipValues[1];
+//     let use_vpn = ipValues[16].is_vpn;
+//     let latitude = ipValues[14];
+//     let longitude = ipValues[15];
 
-    table.push([
-        ip_address,
-        flag,
-        country,
-        city,
-        use_vpn ? chalk.green('Yes') : chalk.red('No'),
-        latitude,
-        longitude
-    ]);
+//     table.push([
+//         ip_address,
+//         flag,
+//         country,
+//         city,
+//         use_vpn ? chalk.green('Yes') : chalk.red('No'),
+//         latitude,
+//         longitude
+//     ]);
 
     
-    // Prints the phone number data table to the console.
-    // This allows the user to see the results of the phone number lookup.
-    console.log(table.toString());
-};
+//     // Prints the phone number data table to the console.
+//     // This allows the user to see the results of the phone number lookup.
+//     console.log(table.toString());
+// };
--- helpers/display.js
+++ helpers/display.js
@@ -7,26 +7,41 @@ import chalk from 'chalk';
 */
 export function displayResults(data) {
     // Display the results in a table
-    const phoneData = [data];
-
+    const info = [data]; // info is an array of objects from phone and IP data
+    const header = info[0];
+    const headerKeys = Object.keys(header).map(key => {
+        // Remove underscores, capitalize, and separate words in the keys
+        return key.replace(/_([a-z])/g, function (match) { return ' ' + match[1].toUpperCase(); }).replace(/^[a-z]/, function (match) { return match.toUpperCase(); });
+    });
+    const headerValues = Object.values(header);
+    // console.log('headerValues: ', headerValues);
+    console.log('headerKeys: ', headerKeys);
     const table = new Table({
-        head: ['Phone Number', 'Country Prefix', 'Location', 'Carrier', 'Type', 'Valid'],
+        head: headerKeys,
         colWidths: [20, 10, 20, 20, 20, 10],
         wordWrap: true,
     });
 
-    // Iterates through the phoneData array and pushes each phone number object's
+    // Iterates through the info array and pushes each phone number object's
     // data into a new row in the table variable to display the results.
-    phoneData.forEach(item => {
-        table.push([
-            item.format.local,
-            item.country.prefix,
-            item.country.name,
-            item.carrier || 'N/A',
-            item.type,
-            item.valid ? chalk.green('Yes') : chalk.red('No'),
-        ]);
+    const keys = Object.keys(info[0]);
+    info.forEach(item => {
+        console.log('item: ', item);
+        const values = Object.values(item);
+        const tableRow = [];
+        keys.forEach((key, index) => {
+            if (key === 'valid') {
+                tableRow.push(values[index] ? chalk.green('Yes') : chalk.red('No'));
+            } else if(key === 'security') {
+                const security = values[index].is_vpn ? chalk.green('Yes') : chalk.red('No');
+                tableRow.push(security);
+            } else {
+                tableRow.push(values[index]);
+            }
+        });
+        table.push(tableRow);
     });
+
 
     // Prints the phone number data table to the console.
     // This allows the user to see the results of the phone number lookup.
